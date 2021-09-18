e7180817b6bdb5ea75e3cc51dad7049bc70078d0
Brandon Ford <Brandon.ford.617@gmail.com>
2021-09-18 19:07:44 -0400

    turned app into pwa

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
   return( 
     <>
         <Head>
-            <title>Next Portfolio</title>
+            <title>Brandons Portfolio</title>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Mukta&display=swap" rel="stylesheet"/>
