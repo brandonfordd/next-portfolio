97451e1d50c2706adee67c63c04368c54fb59525
Brandon Ford <Brandon.ford.617@gmail.com>
2022-09-13 21:26:02 -0400

    npm run build

---
+++ .next/server/pages/api/flux/store.js
@@ -0,0 +1,32 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 110;
+exports.ids = [110];
+exports.modules = {
+
+/***/ 1239:
+/***/ ((module) => {
+
+module.exports = require("events");
+
+/***/ }),
+
+/***/ 8833:
+/***/ ((module) => {
+
+module.exports = require("flux");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [427], () => (__webpack_exec__(7427)));
+module.exports = __webpack_exports__;
+
+})();
