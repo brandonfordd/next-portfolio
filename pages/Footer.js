7326a691ecbcd12a9a048d2daee017063a101c3e
Brandon Ford <Brandon.ford.617@gmail.com>
2022-11-19 14:58:25 -0500

    fix footer being too big

--- pages/Footer.js
+++ pages/Footer.js
@@ -5,7 +5,7 @@ function Footer({ currentPage, handlePageChange }) {
     return (
         <footer className={styles.footer} >
           <div className="text-center p-3">© 2021 Site created by Brandon Ford</div>
-          <img src="https://tracker.metricool.com/c3po.jpg?hash=be122ef3ea6b6b04aa7600ec0697ad16"/>
+          <img className={styles.metriTag} src="https://tracker.metricool.com/c3po.jpg?hash=be122ef3ea6b6b04aa7600ec0697ad16"/>
         </footer>
   );
 }
